4ea48ecc435c52670c56774325b33b8c22da1818
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-11-21 19:27:23 +0530

    09 Printing, Printing, Printing

---
+++ ex9.py
@@ -0,0 +1,20 @@
+# Exercise 9: Printing, Printing, Printing
+# By now you should realize the pattern for this book is to use more than one 
+# exercise to teach you something new. I start with code that you might not 
+# understand, then more exercises explain the concept. If you don't understand something now, 
+# you will later as you complete more exercises. Write down what you don't understand, and keep going.
+
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
