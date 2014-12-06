bd7835cc8fec6a07d10020a8211e0cd6fd0d05b5
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-12-06 09:26:48 +0530

    28 Boolean Practice

---
+++ ex28.py
@@ -0,0 +1,29 @@
+# Exercise 28: Boolean Practice
+# The logic combinations you learned from the last exercise are called "boolean" 
+# logic expressions. Boolean logic is used everywhere in programming. It is an essential 
+# fundamental parts of computation and knowing them very well is akin to knowing your scales in music.
+
+# In this exercise you will take the logic exercises you memorized and start trying them 
+# out in Python. Take each of these logic problems and write what you think the answer will be. 
+# In each case it will be either True or False. Once you have the answers written down, 
+# you will start Python in your Terminal and type each logic problem in to confirm your answers.
+
+
+print True and True
+print False and True
+print 1 == 1 or 2 == 1
+print "test" == "test"
+print 1 == 1 or 2 != 1
+print True and 1 == 1
+print False and 0 != 0
+print True or 1 == 1
+print "test" == "testing"
+print 1 != 0 and 2 == 1
+print not (True and False)
+print not (1 == 1 and 0 != 1)
+print not (10 == 1 or 1000 == 1000)
+print not (1 != 10 or 3 == 4)
+print not ("testing" == "testing" and "rajesh" == "Cool Guy")
+print 1 ==1 and (not ("testing" == 1 or 1 == 0))
+print "chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+print 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
