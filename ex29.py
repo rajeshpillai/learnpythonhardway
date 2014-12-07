a24e1382fd1ac3ce8057955c8a40c64928f7c695
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-12-07 13:07:09 +0530

    29 What If

---
+++ ex29.py
@@ -0,0 +1,31 @@
+# Exercise 29: What If
+# Here is the next script of Python you will enter, 
+# which introduces you to the if-statement. Type this in, make it run exactly right, and then we'll see if your practice has paid off.
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats! The world is doomed!"
+
+if people > cats:
+	print "Not many cats! The world is doomed anyway!"
+
+if people < dogs:
+	print "The world is drooled on!"
+
+if people < dogs:
+	print "The world is dry!"
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+
+if people <= dogs:
+	print "People are less than or equal to dogs."
+
+if people == dogs:
+	print "People are dogs."
+
