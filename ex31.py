6e778baf6401fbeaf5d5acb1c6fab7d33cca3a5a
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-12-09 10:26:34 +0530

    31 Loops and Lists-for-range

--- ex31.py
+++ ex31.py
@@ -18,3 +18,23 @@ change = [1,'pennies',2,'dimes',3,'quarters']
 for number in the_count:
 	print "This is count %d" % number
 
+# also we can go through mixed lists too
+# notice we have use %r since we don't know what's in it
+for i in change:
+	print "I got %r" % i
+
+# we can also build lists, first start with an empty oranges
+elements = []
+
+# then use the range function to do 0 to 5 counts
+for i in range(0, 6):
+	print "Adding %d to the list." % i
+	# append is a function that lists understand
+	elements.append(i)
+
+
+# now we can print them out too
+for i in elements:
+	print "Element was : %d" % i
+
+	
